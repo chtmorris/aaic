c8f03a1d02b07828954cb102bbc3427d7c06be4f
Screen Goose <sarrieschaz@hotmail.com>
2014-08-21 18:04:59 +0800

    Added jobs more people profiles

--- client/main.js
+++ client/main.js
@@ -1,93 +1,93 @@
 // var $ = jQuery;
 // $.noConflict();
-$(document).ready(function() {
-
-    //SMOOTH SCROLL
-    $('.sscroll').bind('click.smoothscroll', function(e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 1200);
-    });
-    //RESPONSIVE VIDEO
-    if($.fn.fitVids){
-        $('.fitvids').fitVids();
-    }
-    //BIG SLIDE
-    $('#home-slide').superslides({
-        animation: 'fade', // You can choose either fade or slide
-        play: 6000,
-        pagination: false
-    });
-    //SERVICES SLIDER
-    $("#slider-services").sudoSlider({
-        speed: 650,
-        auto: true,
-        pause: 3000,
-        prevNext: false,
-        responsive: true,
-        useCSS: true,
-        continuous: true,
-        effect: "slide",
-        updateBefore: true
-    });
-    //PORTFOLIO
-    $('.portfolioContainer').mixitup({
-        filterSelector: '.portfolioFilter a',
-        targetSelector: '.portfolio-item',
-        effects: ['fade', 'scale']
-    });
-    //QUOTE SLIDE
-    $("#quote-slider").sudoSlider({
-        customLink: 'a.quoteLink',
-        speed: 425,
-        prevNext: true,
-        responsive: true,
-        prevHtml: '<a href="#" class="quote-left-indicator"><i class="fa fa-angle-left"></i></a>',
-        nextHtml: '<a href="#" class="quote-right-indicator"><i class="fa fa-angle-right"></i></a>',
-        useCSS: true,
-        continuous: true,
-        effect: "fadeOutIn",
-        updateBefore: true
-    });
-
-    //  Responsive layout, resizing the items
-    if ($('.client-slider').length > 0) {
-        $('.client-slider').carouFredSel({
-            responsive: true,
-            width: '100%',
-            scroll: 1,
-            items: {
-                width: 116,
-                //  height: '30%',  //  optionally resize item-height
-                visible: {
-                    min: 1,
-                    max: 5
-                }
-            }
-        });
-    }
-
-    //BACK TO TOP
-    $("#backtotop").backToTop();
-    //CALL TO ACTION
-    /*
-    var bg_img = $(".call-ta").attr('data-background');
-    $(".call-ta").backstretch(bg_img);
-    */
-    //JQUERY Mobile Devices Responsive
-    $('body').mobileResponsive();
-
-
-    //Bootstrap Tooltip
-    $('a[data-toggle="tooltip"]').tooltip();
-    //NIVOLightbox
-    if($.fn.nivoLightbox){
-        $('.popup').nivoLightbox({
-            effect: 'fall'
-        });
-    }
-});
-$(window).load(function() {
-    $('#loader').fadeOut(1000, "linear");
-});
+// $(document).ready(function() {
+//
+//     //SMOOTH SCROLL
+//     $('.sscroll').bind('click.smoothscroll', function(e) {
+//         e.preventDefault();
+//         $('html,body').animate({
+//             scrollTop: $(this.hash).offset().top
+//         }, 1200);
+//     });
+//     //RESPONSIVE VIDEO
+//     if($.fn.fitVids){
+//         $('.fitvids').fitVids();
+//     }
+//     //BIG SLIDE
+//     $('#home-slide').superslides({
+//         animation: 'fade', // You can choose either fade or slide
+//         play: 6000,
+//         pagination: false
+//     });
+//     //SERVICES SLIDER
+//     $("#slider-services").sudoSlider({
+//         speed: 650,
+//         auto: true,
+//         pause: 3000,
+//         prevNext: false,
+//         responsive: true,
+//         useCSS: true,
+//         continuous: true,
+//         effect: "slide",
+//         updateBefore: true
+//     });
+//     //PORTFOLIO
+//     $('.portfolioContainer').mixitup({
+//         filterSelector: '.portfolioFilter a',
+//         targetSelector: '.portfolio-item',
+//         effects: ['fade', 'scale']
+//     });
+//     //QUOTE SLIDE
+//     $("#quote-slider").sudoSlider({
+//         customLink: 'a.quoteLink',
+//         speed: 425,
+//         prevNext: true,
+//         responsive: true,
+//         prevHtml: '<a href="#" class="quote-left-indicator"><i class="fa fa-angle-left"></i></a>',
+//         nextHtml: '<a href="#" class="quote-right-indicator"><i class="fa fa-angle-right"></i></a>',
+//         useCSS: true,
+//         continuous: true,
+//         effect: "fadeOutIn",
+//         updateBefore: true
+//     });
+//
+//     //  Responsive layout, resizing the items
+//     if ($('.client-slider').length > 0) {
+//         $('.client-slider').carouFredSel({
+//             responsive: true,
+//             width: '100%',
+//             scroll: 1,
+//             items: {
+//                 width: 116,
+//                 //  height: '30%',  //  optionally resize item-height
+//                 visible: {
+//                     min: 1,
+//                     max: 5
+//                 }
+//             }
+//         });
+//     }
+//
+//     //BACK TO TOP
+//     $("#backtotop").backToTop();
+//     //CALL TO ACTION
+//     /*
+//     var bg_img = $(".call-ta").attr('data-background');
+//     $(".call-ta").backstretch(bg_img);
+//     */
+//     //JQUERY Mobile Devices Responsive
+//     $('body').mobileResponsive();
+//
+//
+//     //Bootstrap Tooltip
+//     $('a[data-toggle="tooltip"]').tooltip();
+//     //NIVOLightbox
+//     if($.fn.nivoLightbox){
+//         $('.popup').nivoLightbox({
+//             effect: 'fall'
+//         });
+//     }
+// });
+// $(window).load(function() {
+//     $('#loader').fadeOut(1000, "linear");
+// });
