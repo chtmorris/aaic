35ebe1515d51299518bff0f9e9a558aa44aa7dc3
Screen Goose <sarrieschaz@hotmail.com>
2014-08-25 18:20:55 +0800

    Re-styled contact form

--- client/views/home/8-contact.js
+++ client/views/home/8-contact.js
@@ -2,15 +2,12 @@ Template.contact.rendered = function(){
 
 	dp('#submitButton').click(function(){
 		console.log("clicked");
+		dp('button').addClass( "turn-green" );
+		dp("#submitButton").text(dp("#submitButton").text().replace("Send Message", "Sent"));
 		var emailContents = {};
 		emailContents.email = dp('input[name="email"]').val();
 		emailContents.subject = dp('input[name="subject"]').val();
 		emailContents.message = dp('textarea[name="message"]').val();
-
-		console.log(emailContents);
-
 		Meteor.call('emailContactForm', emailContents);
-
 	});
-
 };
